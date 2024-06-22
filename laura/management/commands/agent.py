afe2cd70aead190de472aa708f4bba00de42fa4b
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-06-22 19:04:16 +0200

    bla

--- laura/management/commands/agent.py
+++ laura/management/commands/agent.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from laura import settings as laura_settings
-from laura.agent import Agent, HumanMessage, Messages
+from laura.agent import Agent, AIMessage, HumanMessage, Messages, SystemMessage
 
 
 class Command(BaseCommand):
@@ -14,6 +14,14 @@ class Command(BaseCommand):
         agent = Agent()
 
         messages = Messages()
+        messages |= SystemMessage("You are Laura, an assistant.")
+        messages |= AIMessage("Hello, I am Laura, your assistant.")
+        messages |= HumanMessage("Hi Laura, how are you?")
+        response = agent.process(laura_settings.LLM_MODEL, messages)
+        print(response)
+
+        return
+
         if options["repl"]:
             while True:
                 message = input("You: ")
@@ -24,6 +32,24 @@ class Command(BaseCommand):
                 print(res)
                 messages |= res
         else:
+            # messages |= SystemMessage(
+            #    f"You are {laura_settings.NAME}, an assistant in a chat group. You must decide if the messages are addressed to you or not. Say YES if the message is addressed to you, NO otherwise."
+            # )
+            messages |= SystemMessage(
+                "You are {laura_settings.NAME}, an assistant in a chat group. You must assists the users in the chat group but most of them doesn't need you. Only answer to the messages that are addressed to you. Say NO_RESPONSE if the message is not addressed to you."
+            )
+
             messages |= HumanMessage("I love programming.")
             res = agent.process(laura_settings.LLM_MODEL, messages)
             print(res)
+            messages |= res
+
+            messages |= HumanMessage("Et les pommes de terre ")
+            res = agent.process(laura_settings.LLM_MODEL, messages)
+            print(res)
+            messages |= res
+
+            messages |= (HumanMessage("Et toi laura, tu aimes quoi ?"),)
+            res = agent.process(laura_settings.LLM_MODEL, messages)
+            print(res)
+            messages |= res
