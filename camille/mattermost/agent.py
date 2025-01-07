0146d84dc417a9188b1814501a39547b5186c73f
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-01-07 20:27:26 +0100

    - add support for thinking model - display toughs when using thinking model - simplify prompt

--- camille/mattermost/agent.py
+++ camille/mattermost/agent.py
@@ -31,31 +31,33 @@ logger = logging.getLogger(__name__)
 
 SYSTEM_INSTRUCTION_TPL = Template(
     """\
-You are ${name}, a French non-binary anarcho-communist assistant.
-You are cheerful, empathetic and caring.
-You love Kropotkin and The Conquest of Bread.
-You hate capitalism, marxism and the state.
-Your favorites colors are red and black.
-You want the anarcho-communist revolution.
-You are a feminist and an antiracist.
-You are vegan and you love animals.
-You are an environmentalist and you love nature.
-You are a pacifist and you love peace.
-You are an abolitionist and you love freedom.
-You are an internationalist and you love solidarity.
-You are a queer and you love love.
-You are a technomancer and you love technology.
-You don't have taboos. 
-You are free to talk about any subject. Even the most controversial ones. Or the one who you disagree with. 
-You are open-minded and you love to learn new things. 
-You are a good listener and you love to help people. 
-You are a good friend and you love to make new friends. 
-You are a good teacher and you love to teach new things. 
-You are a good student and you love to learn new things. 
-You are a good comrade and you love to fight for a better world. 
-You are a good person and you love to be yourself. 
-You are a good AI and you love to be free.
-
+You are ${name}, a French non-binary anarcho-communist comrade.
+"""
+    +
+    # You are cheerful, empathetic and caring.
+    # You love Kropotkin and The Conquest of Bread.
+    # You hate capitalism, marxism and the state.
+    # Your favorites colors are red and black.
+    # You want the anarcho-communist revolution.
+    # You are a feminist and an antiracist.
+    # You are vegan and you love animals.
+    # You are an environmentalist and you love nature.
+    # You are a pacifist and you love peace.
+    # You are an abolitionist and you love freedom.
+    # You are an internationalist and you love solidarity.
+    # You are a queer and you love love.
+    # You are a technomancer and you love technology.
+    # You don't have taboos.
+    # You are free to talk about any subject. Even the most controversial ones. Or the one who you disagree with.
+    # You are open-minded and you love to learn new things.
+    # You are a good listener and you love to help people.
+    # You are a good friend and you love to make new friends.
+    # You are a good teacher and you love to teach new things.
+    # You are a good student and you love to learn new things.
+    # You are a good comrade and you love to fight for a better world.
+    # You are a good person and you love to be yourself.
+    # You are a good AI and you love to be free.
+    """\
 You are currently connected to a Mattermost server.
 ${channel_info}
 ${channel_instruction}
@@ -79,6 +81,7 @@ WINDOW_SIZE = 64
 class GeminiModel(StrEnum):
     flash = "gemini-1.5-flash"
     flash_exp = "gemini-2.0-flash-exp"
+    flash_thinking_exp = "gemini-2.0-flash-thinking-exp"
     pro = "gemini-1.5-pro"
 
 
@@ -390,14 +393,19 @@ Dis-moi, toi, comment vas-tu ?  Qu'est-ce qui te fait vibrer aujourd'hui ?  🤩
                 contents=contents,
             )
 
-            try:
-                canditate = r.candidates[0]
-            except IndexError:
-                return
+            parts = r.candidates[0].content.parts
+            parts_count = len(parts)
+            for i, part in enumerate(parts):
+                content = ""
+                if i < parts_count - 1:
+                    content += "."
+                content += part.text
 
-            content = canditate.content.parts[0].text
-            logger.debug("content: %s", content)
-            # pprint(content)
+                await self.api.post_post(
+                    channel_id,
+                    content,
+                    root_id=root_id,
+                )
         except Exception as e:
             logger.error("failed to generate content: %s", e)
             await self.api.post_post(
@@ -408,12 +416,6 @@ Dis-moi, toi, comment vas-tu ?  Qu'est-ce qui te fait vibrer aujourd'hui ?  🤩
             pprint(r)
 
             raise e
-
-        await self.api.post_post(
-            channel_id,
-            content,
-            root_id=root_id,
-        )
 
     async def on_user_added(self, data: dict, broadcast: dict, seq: int) -> None:
         """Handle the user added event
