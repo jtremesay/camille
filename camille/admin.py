0f51bcbac98178bdf812b0fcd66fafd158662ef3
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-05-09 12:25:36 +0200

    add model admin

--- camille/admin.py
+++ camille/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from camille.models import MMChannel, MMMembership, MMTeam, MMUser
+
+
+@admin.register(MMTeam)
+class MMTeamAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "display_name")
+
+
+class MembershipInline(admin.TabularInline):
+    model = MMMembership
+    extra = 0
+
+
+@admin.register(MMChannel)
+class MMChannelAdmin(admin.ModelAdmin):
+    list_display = ("id", "team__name", "type", "name", "display_name")
+    inlines = [MembershipInline]
+
+
+@admin.register(MMUser)
+class MMUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "nickname", "first_name", "last_name")
+    inlines = [MembershipInline]
