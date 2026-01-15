d077a2e5a50545261a03d5bde29635e10c369a89
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-01-15 22:35:39 +0100

    can register llm creds

--- src/camille/admin.py
+++ src/camille/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class UserAWSBedrockInferenceProviderCredentialsInline(admin.StackedInline):
+    model = models.UserAWSBedrockInferenceProviderCredentials
+    extra = 0
+
+
+class UserGoogleGLAInferenceProviderCredentialsInline(admin.StackedInline):
+    model = models.UserGoogleGLAInferenceProviderCredentials
+    extra = 0
+
+
+class UserMistralAIInferenceProviderCredentialsInline(admin.StackedInline):
+    model = models.UserMistralAIInferenceProviderCredentials
+    extra = 0
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user")
+    inlines = [
+        UserAWSBedrockInferenceProviderCredentialsInline,
+        UserGoogleGLAInferenceProviderCredentialsInline,
+        UserMistralAIInferenceProviderCredentialsInline,
+    ]
+
+
+@admin.register(models.UserAWSBedrockInferenceProviderCredentials)
+class UserAWSBedrockInferenceProviderCredentialsAdmin(admin.ModelAdmin):
+    list_display = ("id", "profile", "region_name")
+
+
+@admin.register(models.UserGoogleGLAInferenceProviderCredentials)
+class UserGoogleGLAInferenceProviderCredentialsAdmin(admin.ModelAdmin):
+    list_display = ("id", "profile")
+
+
+@admin.register(models.UserMistralAIInferenceProviderCredentials)
+class UserMistralAIInferenceProviderCredentialsAdmin(admin.ModelAdmin):
+    list_display = ("id", "profile")
--- src/camille/models.py
+++ src/camille/models.py
@@ -1,3 +1,61 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class InferenceProvider(models.TextChoices):
+    AWS_BEDROCK = "aws_bedrock", "AWS Bedrock"
+    GOOGLE_GLA = "google_gla", "Google Generative AI"
+    MISTRAL_AI = "mistral_ai", "Mistral AI"
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile_on_user_create(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+class UserInferenceProviderCredentials(models.Model):
+    class Meta:
+        unique_together = ("profile", "provider")
+        abstract = True
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    provider = models.CharField(
+        max_length=50,
+        choices=InferenceProvider.choices,
+    )
+
+
+class UserAWSBedrockInferenceProviderCredentials(UserInferenceProviderCredentials):
+    api_key = models.TextField()
+    region_name = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        self.provider = InferenceProvider.AWS_BEDROCK
+        super().save(*args, **kwargs)
+
+
+class UserGoogleGLAInferenceProviderCredentials(UserInferenceProviderCredentials):
+    api_key = models.TextField()
+
+    def save(self, *args, **kwargs):
+        self.provider = InferenceProvider.GOOGLE_GLA
+        super().save(*args, **kwargs)
+
+
+class UserMistralAIInferenceProviderCredentials(UserInferenceProviderCredentials):
+    api_key = models.TextField()
+
+    def save(self, *args, **kwargs):
+        self.provider = InferenceProvider.MISTRAL_AI
+        super().save(*args, **kwargs)
