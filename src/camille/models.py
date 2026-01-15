1992d98a6527067b00f0cb6a6ab6a1513d3f42cc
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-01-15 22:38:57 +0100

    add types

--- src/camille/models.py
+++ src/camille/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -14,12 +15,14 @@ class InferenceProvider(models.TextChoices):
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.user.username
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_user_profile_on_user_create(sender, instance, created, **kwargs):
+def create_user_profile_on_user_create(
+    sender: type[AbstractBaseUser], instance: AbstractBaseUser, created: bool, **kwargs
+) -> None:
     if created:
         Profile.objects.create(user=instance)
 
@@ -40,7 +43,7 @@ class UserAWSBedrockInferenceProviderCredentials(UserInferenceProviderCredential
     api_key = models.TextField()
     region_name = models.CharField(max_length=100)
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         self.provider = InferenceProvider.AWS_BEDROCK
         super().save(*args, **kwargs)
 
@@ -48,7 +51,7 @@ class UserAWSBedrockInferenceProviderCredentials(UserInferenceProviderCredential
 class UserGoogleGLAInferenceProviderCredentials(UserInferenceProviderCredentials):
     api_key = models.TextField()
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         self.provider = InferenceProvider.GOOGLE_GLA
         super().save(*args, **kwargs)
 
@@ -56,6 +59,15 @@ class UserGoogleGLAInferenceProviderCredentials(UserInferenceProviderCredentials
 class UserMistralAIInferenceProviderCredentials(UserInferenceProviderCredentials):
     api_key = models.TextField()
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         self.provider = InferenceProvider.MISTRAL_AI
         super().save(*args, **kwargs)
+
+
+class MattermostServer(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+    token = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
