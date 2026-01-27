517b616e8d35f65be2e34626805b9e6f2c9db881
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-01-27 14:37:52 +0100

    try to fix crash by using explicit async

--- camille/ai/prompts.py
+++ camille/ai/prompts.py
@@ -1,11 +1,12 @@
 from pydantic_ai import RunContext
 
 from camille.ai.deps import Dependency
+from camille.models import PersonalityPrompt
 
-
-def personality_prompt(ctx: RunContext[Dependency]) -> str:
-    prompt = ctx.deps.sender.prompt
-    if prompt is None:
+async def personality_prompt(ctx: RunContext[Dependency]) -> str:
+    prompt_id = ctx.deps.sender.prompt_id
+    if prompt_id is None:
         return ""
 
+    prompt = await PersonalityPrompt.objects.get(id=prompt_id)
     return prompt.prompt_template.format(agent_name=ctx.deps.me.first_name)
