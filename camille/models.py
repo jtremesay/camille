cbdded4e85f46f43a030909a8d736def996abafe
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-02-03 18:14:18 +0100

    create profile model

--- camille/models.py
+++ camille/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+        related_query_name="profile",
+    )
+
+
+# Auto-create a profile when a new user is created
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
