533c57e306b13ccc6dda2feea198f70b1e7920e5
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-02-04 01:01:25 +0100

    refacto client

--- src/mattermost.py
+++ src/mattermost.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from typing import Optional
 
-from django.conf import settings
 from httpx import AsyncClient
 from httpx_ws import AsyncWebSocketSession, aconnect_ws
 from pydantic import BaseModel, TypeAdapter
@@ -60,41 +59,120 @@ class ChannelMember(BaseModel):
 ChannelMemberList = TypeAdapter(list[ChannelMember])
 
 
-class Mattermost:
-    def __init__(
-        self,
-        base_url: str,
-        token: str,
-    ):
-        self._http_client = AsyncClient(
-            base_url=base_url + "/api/v4/",
-            headers={
-                "Authorization": f"Bearer {token}",
-                "User-Agent": "CamilleBot/1.0",
-            },
+class ModelClient:
+    base_url: str
+
+    def __init__(self, http_client: AsyncClient) -> None:
+        self._http_client = http_client
+
+    async def _get(self, url: str):
+        full_url = self.get_full_url(url)
+
+        response = await self._http_client.get(full_url)
+        response.raise_for_status()
+
+        return response
+
+    async def _post(self, url: str, json: dict):
+        full_url = self.get_full_url(url)
+
+        response = await self._http_client.post(full_url, json=json)
+        response.raise_for_status()
+
+        return response
+
+    def get_base_url(self) -> str:
+        return self.base_url
+
+    def get_full_url(self, url: str) -> str:
+        full_url = self.get_base_url()
+        if url:
+            full_url += "/" + url
+        return full_url
+
+
+class ChannelClient(ModelClient):
+    base_url = "channels"
+
+    async def get(self, channel_id: str) -> Channel:
+        response = await self._get(channel_id)
+        return Channel.model_validate_json(response.content)
+
+    async def list_members(self, channel_id: str) -> list[ChannelMember]:
+        response = await self._get(f"{channel_id}/members")
+        response.raise_for_status()
+        return ChannelMemberList.validate_json(response.content)
+
+
+class UserClient(ModelClient):
+    base_url = "users"
+
+    async def list(self) -> list[User]:
+        response = await self._get("")
+
+        return UserList.validate_json(response.content)
+
+    async def get(self, user_id: str) -> User:
+        response = await self._get(user_id)
+
+        return User.model_validate_json(response.content)
+
+    async def get_me(self) -> User:
+        return await self.get("me")
+
+    async def get_teams(self, user_id: str) -> list[Team]:
+        response = await self._get(f"{user_id}/teams")
+        response.raise_for_status()
+
+        return TeamList.validate_json(response.content)
+
+    async def get_channels_for_user(self, user_id: str, team_id: str) -> list[Channel]:
+        response = await self._get(f"{user_id}/teams/{team_id}/channels")
+        response.raise_for_status()
+
+        return ChannelList.validate_json(response.content)
+
+
+class PostClient(ModelClient):
+    base_url = "posts"
+
+    async def post(self, channel_id: str, message: str, root_id: Optional[str] = None):
+        response = await self._post(
+            "",
+            json={"channel_id": channel_id, "root_id": root_id, "message": message},
         )
+        response.raise_for_status()
+
+
+class FileClient(ModelClient):
+    base_url = "files"
+
+    async def get_file(self, file_id: str) -> bytes:
+        response = await self._get(file_id)
+        response.raise_for_status()
+
+        return response.content
+
+
+class WebSocketClient:
+    def __init__(self, http_client: AsyncClient):
+        self._http_client = http_client
         self._ws_client: Optional[AsyncWebSocketSession] = None
         self.current_seq = 0
 
-    @classmethod
-    def create(cls):
-        return cls(settings.MATTERMOST_HOST, settings.MATTERMOST_API_TOKEN)
-
-    async def __aenter__(self):
-        await self._http_client.__aenter__()
-
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self._http_client.__aexit__(exc_type, exc_val, exc_tb)
-
-    async def connect(
-        self,
-    ):
-        async with aconnect_ws("websocket", self._http_client) as ws:
+    async def connect(self, timeout: Optional[float] = None):
+        async with aconnect_ws(
+            "websocket",
+            self._http_client,
+        ) as ws:
             self._ws_client = ws
             while True:
-                event = await self._ws_client.receive_json()
+                try:
+                    event = await self._ws_client.receive_json(timeout=timeout)
+                except TimeoutError:
+                    yield "timeout", None, None, None
+                    continue
+
                 kind = event.get("event")
                 if not kind:
                     continue
@@ -107,7 +185,7 @@ class Mattermost:
                 seq = event["seq"]
                 self.current_seq = max(self.current_seq, seq)
 
-                await self.on_event(kind, data, broadcast, seq)
+                yield kind, data, broadcast, seq
 
     async def on_event(self, kind: str, data, broadcast, seq):
         if event_handler := getattr(self, f"on_{kind}", None):
@@ -120,54 +198,7 @@ class Mattermost:
                 f"Unhandled event: {kind}, Data: {data}, Broadcast: {broadcast}, Seq: {seq}"
             )
 
-    async def get_users(self) -> list[User]:
-        response = await self._http_client.get("users")
-        response.raise_for_status()
-        return UserList.validate_json(response.content)
-
-    async def get_user(self, user_id: str) -> User:
-        response = await self._http_client.get(f"users/{user_id}")
-        response.raise_for_status()
-        return User.model_validate_json(response.content)
-
-    async def get_me(self) -> User:
-        return await self.get_user("me")
-
-    async def get_teams(self, user_id: str) -> list[Team]:
-        response = await self._http_client.get(f"users/{user_id}/teams")
-        response.raise_for_status()
-        return TeamList.validate_json(response.content)
-
-    async def get_channels_for_user(self, user_id: str, team_id: str) -> list[Channel]:
-        response = await self._http_client.get(
-            f"users/{user_id}/teams/{team_id}/channels"
-        )
-        response.raise_for_status()
-        return ChannelList.validate_json(response.content)
-
-    async def get_channel(self, channel_id: str) -> Channel:
-        response = await self._http_client.get(f"channels/{channel_id}")
-        response.raise_for_status()
-        return Channel.model_validate_json(response.content)
-
-    async def get_channel_members(self, channel_id: str) -> list[ChannelMember]:
-        response = await self._http_client.get(f"channels/{channel_id}/members")
-        response.raise_for_status()
-        return ChannelMemberList.validate_json(response.content)
-
-    async def post_message(self, channel_id: str, root_id: str, message: str):
-        response = await self._http_client.post(
-            "posts",
-            json={"channel_id": channel_id, "root_id": root_id, "message": message},
-        )
-        response.raise_for_status()
-
-    async def get_file(self, file_id: str) -> bytes:
-        response = await self._http_client.get(f"files/{file_id}")
-        response.raise_for_status()
-        return response.content
-
-    async def ws_send(self, action: str, data: dict) -> None:
+    async def send(self, action: str, data: dict) -> None:
         assert self._ws_client is not None
 
         self.current_seq += 1
@@ -180,4 +211,33 @@ class Mattermost:
         )
 
     async def user_typing(self, channel_id: str) -> None:
-        await self.ws_send("user_typing", {"channel_id": channel_id})
+        await self.send("user_typing", {"channel_id": channel_id})
+
+
+class Mattermost:
+    def __init__(
+        self,
+        base_url: str,
+        api_token: str,
+    ):
+        self._http_client = AsyncClient(
+            base_url=base_url + "/api/v4/",
+            headers={
+                "Authorization": f"Bearer {api_token}",
+                "User-Agent": "CamilleBot/1.0",
+            },
+        )
+
+        self.users = UserClient(self._http_client)
+        self.channels = ChannelClient(self._http_client)
+        self.posts = PostClient(self._http_client)
+        self.files = FileClient(self._http_client)
+        self.ws = WebSocketClient(self._http_client)
+
+    async def __aenter__(self):
+        await self._http_client.__aenter__()
+
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self._http_client.__aexit__(exc_type, exc_val, exc_tb)
