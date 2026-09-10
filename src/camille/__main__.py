6d77a119f5f054549a8c3f89f5a7196c361581e6
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-09-10 19:04:57 +0200

    add license

--- src/camille/__main__.py
+++ src/camille/__main__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Camille - An AI assistant
 # Copyright (C) Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
 #
@@ -14,8 +15,23 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-from django.apps import AppConfig
+import os
+import sys
 
 
-class CamilleConfig(AppConfig):
-    name = "camille"
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "camille.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
--- src/camille/urls.py
+++ src/camille/urls.py
@@ -1,20 +1,18 @@
-"""
-URL configuration for camille project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+# Camille - An AI assistant
+# Copyright (C) Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 from django.contrib import admin
 from django.urls import path
 
